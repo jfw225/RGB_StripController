e4150294431f2a12e77df8641ac41253ce773e38
Joseph Whelan <jfw225@cornell.edu>
2021-04-09 03:29:59 -0400

    Update controller.ino

--- controller/controller.ino
+++ controller/controller.ino
@@ -3,18 +3,36 @@
 #define GREENPIN 3
 int pins[] = {REDPIN, GREENPIN, BLUEPIN};
 
+/* Sets each color pin to OUTPUT. */
+void init_pins() {
+  for (int i = 0; i < 3; i++ ) pinMode(pins[i], OUTPUT);
+}
+
+/* Converts serial input to an [r, g, b] list. */
+void parse_input(String inp, int rgb[3]) {
+  int c = 0;
+  String acc;
+  for (int i = 0; i < inp.length(); i++ ) {
+    char temp = inp[i];
+    if (temp == ',') {
+      rgb[c] = acc.toInt();
+      Serial.println(acc);
+      c++ ;
+      acc = "";
+    } else acc += temp;
+  }
+}
+
 /* Pass in list [r, g, b]. */
 void write_color(int colors[3]) {
   for (int i = 0; i < 3; i++ ) {
     analogWrite(pins[i], colors[i]);
   }
 }
+
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
-  for (int i = 0; i < 3; i++ ) {
-    pinMode(pins[i], OUTPUT);
-  }
+  init_pins();
 }
 
 // 255, 40, 0
@@ -23,12 +41,10 @@ int color1[3] = {255, 40, 0};
 int color2[3] = {255, 242, 0};
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  while ( Serial.available() ) {
+    int rgb[3] = {};
+    parse_input(Serial.readString(), rgb);
+    write_color(rgb);
+  } 
   
-//  analogWrite(3, 255);
-  write_color(color1);
-  delay(500);
-//  analogWrite(3, 0);
-  write_color(color2);
-  delay(500);
 }
